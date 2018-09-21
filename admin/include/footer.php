311b3f4f662d2c9359c6a68d5212fecb85297b19
mukeshgoraigs <mukeshgorai24@gmail.com>
2018-09-21 22:36:44 +0530

    add new project

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">Complaint Management System 2018</b>  
+		</div>
+	</div>
---
+++ users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+              Complaint Management System 2018
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
